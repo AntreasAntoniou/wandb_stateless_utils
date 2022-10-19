b73cc162e3681f6c221ac4dd25bebbf871ffb17f
Antreas Antoniou <antrikohs@gmail.com>
2022-10-19 22:43:50 +0100

    Update checkpointing.py

--- wandbless/checkpointing.py
+++ wandbless/checkpointing.py
@@ -1,61 +1,107 @@
 import pathlib
-from typing import Dict, Union
+from typing import Any, Dict, Optional, Sequence, Union
 
 import torch
 import wandb
 from torch import nn
 
 
-def save(
-    model: nn.ModuleDict,
-    model_name: str,
-    store_dir: Union[str, pathlib.Path],
-):
-    torch.save(model.state_dict(), store_dir)
-    checkpoint_object = wandb.Artifact(
-        type="model", name=f"exp-{wandb.run.id}.model-{model_name}"
-    )
-    checkpoint_object.add_file(store_dir)
-    wandb.log_artifact(checkpoint_object)
+class StatelessCheckpointingWandb:
+    def __init__(
+        self,
+        job_type: Optional[str] = None,
+        dir: Union[str, pathlib.Path, None] = None,
+        config: Union[Dict, str, None] = None,
+        project: Optional[str] = None,
+        entity: Optional[str] = None,
+        reinit: bool = None,
+        tags: Optional[Sequence] = None,
+        group: Optional[str] = None,
+        name: Optional[str] = None,
+        notes: Optional[str] = None,
+        magic: Union[dict, str, bool] = None,
+        config_exclude_keys=None,
+        config_include_keys=None,
+        anonymous: Optional[str] = None,
+        mode: Optional[str] = None,
+        allow_val_change: Optional[bool] = None,
+        force: Optional[bool] = None,
+        tensorboard=None,
+        sync_tensorboard=None,
+        monitor_gym=None,
+        save_code=None,
+        id=None,
+        settings: Union[wandb.Settings, Dict[str, Any], None] = None,
+    ):
+        self.run = wandb.init(
+            job_type=job_type,
+            dir=dir,
+            config=config,
+            project=project,
+            entity=entity,
+            reinit=reinit,
+            tags=tags,
+            group=group,
+            name=name,
+            notes=notes,
+            magic=magic,
+            config_exclude_keys=config_exclude_keys,
+            config_include_keys=config_include_keys,
+            anonymous=anonymous,
+            mode=mode,
+            allow_val_change=allow_val_change,
+            force=force,
+            tensorboard=tensorboard,
+            sync_tensorboard=sync_tensorboard,
+            monitor_gym=monitor_gym,
+            save_code=save_code,
+            id=id,
+            settings=settings,
+            resume="allow",
+        )
 
+    def save(
+        self,
+        model: nn.ModuleDict,
+        model_name: str,
+        store_dir: Union[str, pathlib.Path],
+    ):
+        torch.save(model.state_dict(), pathlib.Path(store_dir) / "checkpoint.pth")
+        checkpoint_object = wandb.Artifact(
+            type="model", name=f"exp-{self.run.id}.model-{model_name}"
+        )
+        checkpoint_object.add_file(store_dir)
+        wandb.log_artifact(checkpoint_object)
 
-def restore(
-    project: str,
-    entity: str,
-    id: str,
-    dir: Union[str, pathlib.Path],
-    model_name: str,
-    version: str,
-):
-    run = wandb.init()
-    artifact = run.use_artifact(
-        f"{entity}/{project}/exp-{id}.model-{model_name}:{version}", type="model"
-    )
-    artifact_dir = artifact.download(root=dir)
+    def restore(
+        self,
+        store_dir: Union[str, pathlib.Path],
+        model_name: str,
+        version: str,
+    ):
+        artifact = self.run.use_artifact(
+            f"{self.run.entity}/{self.run.project}/exp-{id}.model-{model_name}:{version}",
+            type="model",
+        )
+        artifact_dir = artifact.download(root=store_dir)
 
-    latest_model = torch.load(pathlib.Path(artifact_dir) / "checkpoint.pth")
+        latest_model = torch.load(pathlib.Path(artifact_dir) / "checkpoint.pth")
 
-    return latest_model
+        return latest_model
 
+    def restore_latest(
+        self,
+        store_dir: Union[str, pathlib.Path],
+        model_name: str,
+    ):
 
-def restore_latest(
-    project: str,
-    entity: str,
-    id: str,
-    dir: Union[str, pathlib.Path],
-    model_name: str,
-):
+        return self.restore(store_dir, model_name, version="latest")
 
-    return restore(project, entity, id, dir, model_name, version="latest")
+    def restore_epoch(
+        self,
+        store_dir: Union[str, pathlib.Path],
+        model_name: str,
+        epoch: int,
+    ):
 
-
-def restore_epoch(
-    project: str,
-    entity: str,
-    id: str,
-    store_dir: Union[str, pathlib.Path],
-    model_name: str,
-    epoch: int,
-):
-
-    return restore(project, entity, id, store_dir, model_name, version=f"v{epoch}")
+        return self.restore(store_dir, model_name, version=f"v{epoch}")
